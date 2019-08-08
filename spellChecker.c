2a50eb5a8fc265803f0606285131f93e1174b9b5
sn3aks <52215761+sn3aks@users.noreply.github.com>
2019-08-08 10:02:27 -0700

    Add files via upload
    Here's what I've got for this...might be good...idk

--- spellChecker.c
+++ spellChecker.c
@@ -54,8 +54,61 @@ char* nextWord(FILE* file)
  */
 void loadDictionary(FILE* file, HashMap* map)
 {
+    char* word = nextWord(file);
+    while(word != NULL){
+        int idx = abs(HASH_FUNCTION(word)) % map->capacity;
+        for(int i = 0; i < map->capacity; i++){
+            if(i == idx){
+                hashMapPut(map, word, /*value*/);
+            }
+        }
+        word = nextWord(file);
+    }
     // FIXME: implement
 }
+
+int minMoves(int a, int b, int c){
+    if(a < b && a < c){
+        return a;
+    }
+    if(b < a && b < c){
+        return b;
+    }
+    return c;
+}
+
+HashLink* leDist(const char* input, const char* key){   //returns the pointer to the link that contains the matching word?
+    int inLen, keyLen;
+    inLen = strlen(input);
+    keyLen = strlen(key);
+
+    int mtrx[inLen + 1][keyLen + 1];
+    for(int i = 0; i <= inLen; i ++){   //rows
+        mtrx[i][0] = i;
+    }
+    for(int i = 0; i <= keyLen; i ++){  //columns
+        mtrx[0][i] = i;
+    }
+    for(int i = 1; i <= inLen; i++){
+        char c1;
+        c1 = input[i - 1];
+        for(int j = 1; j <= keyLen; j++){
+            char c2;
+            c2 = key[j - 1];
+            if(c1 == c2){
+                mtrx[i][j] = mtrx[i - 1][j - 1];
+            }
+            else{
+                int del = mtrx[i - 1][j] + 1;
+                int ins = mtrx[i][j - 1] + 1;
+                int sub = mtrx[i - 1][j - 1] + 1;
+                mtrx[i][j] = minMoves(del, ins, sub);
+            }
+        }
+    }
+
+}
+
 
 /**
  * Prints the concordance of the given file and performance information. Uses
@@ -69,29 +122,74 @@ int main(int argc, const char** argv)
 {
     // FIXME: implement
     HashMap* map = hashMapNew(1000);
-    
+    HashLink* cur1, cur2;
+    int moves;
+
     FILE* file = fopen("dictionary.txt", "r");
     clock_t timer = clock();
     loadDictionary(file, map);
     timer = clock() - timer;
     printf("Dictionary loaded in %f seconds\n", (float)timer / (float)CLOCKS_PER_SEC);
     fclose(file);
-    
+
     char inputBuffer[256];
     int quit = 0;
     while (!quit)
     {
         printf("Enter a word or \"quit\" to quit: ");
         scanf("%s", inputBuffer);
-        
+        char tmp;
+        while(inputBuffer){         //puts user inputted word to lower case to aid comparisons
+            tolower(tmp);
+        }
+
+        int matching;
+        for(int i = 0; i < map->capacity; i++){     //iterates through all buckets(sentinels)
+            cur1 = map->table[i]->next;
+            while(cur1 != NULL){
+                moves = leDist(word, cur1->key);  //not sure how to catch the return...maybe a hash map link
+                if(moves == 0){
+                    matching = 1;
+                    printf("The word "%s" is spelled correctly. \n", inputBuffer);
+                }
+                else{
+                    matching = 0;
+                    cur1->value = moves;
+                }
+                cur1 = cur1->next;
+            }
+        }
+
+        if(matching == 0){
+            int count = 0;
+            HashLink* minArr[5];        //creates hashlink array to hold the locations of the five closest words
+            for(int j = 0; j < map->capacity; j++){
+                cur2 = map->table[j]->next;
+                while(cur2 != NULL){
+                    if(count < 5){      //fills the array with the first five words
+                        minArr[count] = cur2;
+                        count++;
+                    }
+                    else{
+                        for(int k = 0; k < 5; k++){     //compares every word distance with distances in the array
+                            if(cur2->value < minArr[k]){
+                                minArr[k] = cur2;       //once a new distance is placed in an array location, jump out of array comparison loop
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            printf("The inputted word:%s, is spelled incorrectly, did you mean; %s, %s, %s, %s, %s?\n", inputBuffer, minArr[0], minArr[1], minArr[2], minArr[3], minArr[4]);
+        }
         // Implement the spell checker code here..
-        
+
         if (strcmp(inputBuffer, "quit") == 0)
         {
             quit = 1;
         }
     }
-    
+
     hashMapDelete(map);
     return 0;
 }
